0c89830d27b647e98b46ac796f54d5ce25a502e3
Shane Arceo <arceoshane23@gmail.com>
2025-03-25 12:53:08 -0400

    improved validation by using Pydantic

--- api/api.py
+++ api/api.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, UUID4
+from psycopg.rows import class_row
 import psycopg
 import os
 
 app = FastAPI()
+
+
+class Title(BaseModel):
+    id: UUID4  # id stored as version 4 UUID in PostgreSQL
+    tconst: str
+    title_type_id: UUID4
+    primary_title: str | None = None
+    original_title: str | None = None
+    is_adult: bool | None = None
+    start_year: int | None = None
+    end_year: int | None = None
+    runtime_minutes: int | None = None
 
 
 @app.get("/titles")
@@ -11,28 +25,28 @@ async def root(id_="", offset="0", limit="10"):
         with psycopg.connect(
             f"host=localhost port={os.environ.get('PG_PORT')} connect_timeout=10 user={os.environ.get('PG_LOCAL_USER')} password={os.environ.get('PG_LOCAL_PASSWORD')}"
         ) as conn:
-            with conn.cursor() as cur:
+            with conn.cursor(row_factory=class_row(Title)) as cur:
                 if not id_:
-                    records = cur.execute(
+                    cur = cur.execute(
                         """
-                            SELECT id, primary_title
+                            SELECT id, tconst, title_type_id, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes
                             FROM title
                             OFFSET %s
                             LIMIT %s;
                         """,
                         [offset, limit],
                     )
-                    return [{"id": record[0], "title": record[1]} for record in records]
+                    return [record for record in cur]
                 else:
-                    records = cur.execute(
+                    cur = cur.execute(
                         """
-                            SELECT id, primary_title
+                            SELECT id, tconst, title_type_id, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes
                             FROM title
                             WHERE id = %s;
                         """,
                         [id_],
                     )
-                    return [{"id": record[0], "title": record[1]} for record in records]
+                    return cur.fetchone()
 
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
