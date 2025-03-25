4fb0f592b229e35a8519ad8ba5b674138e85332c
Shane Arceo <arceoshane23@gmail.com>
2025-03-25 15:16:42 -0400

    created separate functions for single title and titles; updated typing to take advantage of fastapi features

--- api/api.py
+++ api/api.py
@@ -19,34 +19,44 @@ class Title(BaseModel):
     runtime_minutes: int | None = None
 
 
-@app.get("/titles")
-async def root(id_="", offset="0", limit="10"):
+@app.get("/titles/{id_}")
+async def get_title(id_: UUID4) -> Title:
     try:
         with psycopg.connect(
             f"host=localhost port={os.environ.get('PG_PORT')} connect_timeout=10 user={os.environ.get('PG_LOCAL_USER')} password={os.environ.get('PG_LOCAL_PASSWORD')}"
         ) as conn:
             with conn.cursor(row_factory=class_row(Title)) as cur:
-                if not id_:
-                    cur = cur.execute(
-                        """
+                cur = cur.execute(
+                    """
+                        SELECT id, tconst, title_type_id, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes
+                        FROM title
+                        WHERE id = %s;
+                    """,
+                    [id_],
+                )
+                return cur.fetchone()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+@app.get("/titles")
+async def get_titles(offset: int = 0, limit: int = 10) -> list[Title]:
+    try:
+        with psycopg.connect(
+            f"host=localhost port={os.environ.get('PG_PORT')} connect_timeout=10 user={os.environ.get('PG_LOCAL_USER')} password={os.environ.get('PG_LOCAL_PASSWORD')}"
+        ) as conn:
+            with conn.cursor(row_factory=class_row(Title)) as cur:
+                cur = cur.execute(
+                    """
                             SELECT id, tconst, title_type_id, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes
                             FROM title
                             OFFSET %s
                             LIMIT %s;
                         """,
-                        [offset, limit],
-                    )
-                    return [record for record in cur]
-                else:
-                    cur = cur.execute(
-                        """
-                            SELECT id, tconst, title_type_id, primary_title, original_title, is_adult, start_year, end_year, runtime_minutes
-                            FROM title
-                            WHERE id = %s;
-                        """,
-                        [id_],
-                    )
-                    return cur.fetchone()
+                    [offset, limit],
+                )
+                return [record for record in cur]
 
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
